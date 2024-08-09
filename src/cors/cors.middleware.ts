4b5d76e511c0a22b7441f042b57e594c31ef02de
ricqcodes <princenwakanma1996@gmail.com>
2024-08-09 08:57:58 +0100

    update cors

--- src/cors/cors.middleware.ts
+++ src/cors/cors.middleware.ts
@@ -8,11 +8,16 @@ import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
+  allowedOrigins = [
+    'https://intelytics-fe.vercel.app',
+    'http://localhost:3000',
+  ];
+
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', [
-      'https://intelytics-fe.vercel.app',
-      'http://localhost:3000',
-    ]);
+    const origin = req.headers.origin;
+    if (this.allowedOrigins.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: ['https://intelytics-fe.vercel.app', 'http://localhost:3000'], // Your Next.js app's URL
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
   await app.listen(3001);
 }
 bootstrap();
