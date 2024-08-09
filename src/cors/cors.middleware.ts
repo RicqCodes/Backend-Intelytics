de3cda09350ae0d4820c9a2e3f74567d5054aa9b
ricqcodes <princenwakanma1996@gmail.com>
2024-08-09 08:48:17 +0100

    update url route

--- src/cors/cors.middleware.ts
+++ src/cors/cors.middleware.ts
@@ -9,7 +9,10 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', [
+      'https://intelytics-fe.vercel.app',
+      'http://localhost:3000',
+    ]);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
